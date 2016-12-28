da59db05beb0f51540916e874ef1d200e2de1977
EomJaeWoong <zzang6535@gmail.com>
2016-12-28 11:18:38 +0900

    조금 수정
    테스트 부분 쓰레드로 수정

--- 16.12.19_MemoryPool/test.cpp
+++ 16.12.19_MemoryPool/test.cpp
@@ -1,19 +1,72 @@
-#include "MemoryPool.h"
 #include <stdio.h>
+#include <process.h>
+#include <Windows.h>
+
+#include "MemoryPool.h"
+
+#define dfTHREAD_MAX 10
+
+CMemoryPool<int> g_mem(0);
+
+
+unsigned __stdcall StackThread(void *pParam);
 
 void main()
 {
-	CMemoryPool<int> m(10000);
+	HANDLE hThread[dfTHREAD_MAX];
+	DWORD dwThreadID;
+
+
+
+	for (int iCnt = 0; iCnt < dfTHREAD_MAX; iCnt++)
+	{
+		hThread[iCnt] = (HANDLE)_beginthreadex(
+			NULL,
+			0,
+			StackThread,
+			(LPVOID)1000,
+			0,
+			(unsigned int *)&dwThreadID
+			);
+	}
+
+	while (1)
+	{
+		Sleep(999);
+	}
+}
+
+/*------------------------------------------------------------------*/
+// 0. 각 스레드에서 st_QUEUE_DATA 데이터를 일정 수치 (10000개) 생성		
+// 0. 데이터 생성(확보)
+// 1. iData = 0x0000000055555555 셋팅
+// 1. lCount = 0 셋팅
+// 2. 스택에 넣음
+
+// 3. 약간대기  Sleep (0 ~ 3)
+// 4. 내가 넣은 데이터 수 만큼 뽑음 
+// 4. - 이때 뽑히는건 내가 넣은 데이터일 수도, 다른 스레드가 넣은 데이터일 수도 있음
+// 5. 뽑은 전체 데이터가 초기값과 맞는지 확인. (데이터를 누가 사용하는지 확인)
+// 6. 뽑은 전체 데이터에 대해 lCount Interlock + 1
+// 6. 뽑은 전체 데이터에 대해 iData Interlock + 1
+// 7. 약간대기
+// 8. + 1 한 데이터가 유효한지 확인 (뽑은 데이터를 누가 사용하는지 확인)
+// 9. 데이터 초기화 (0x0000000055555555, 0)
+// 10. 뽑은 수 만큼 스택에 다시 넣음
+//  3번 으로 반복.
+/*------------------------------------------------------------------*/
+unsigned __stdcall StackThread(void *pParam)
+{
 	int *i;
 
 	while (1)
 	{
-		i = m.Alloc();
+		i = g_mem.Alloc();
 
 		*i = rand();
 		printf("%d\n", *i);
-		m.Free(i);
+		g_mem.Free(i);
 
-		//Sleep(50);
+		Sleep(3);
 	}
 }
